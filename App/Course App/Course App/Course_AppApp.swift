b853160cafae52de33c1f89a27cea2f03e19bf5f
Peter Surovy <psurovy@gmail.com>
2024-04-27 00:32:55 +0200

    Create .gitkeep

---
+++ App/Course App/Course App/Course_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_AppApp.swift
+//  Course App
+//
+//  Created by Peter Surovy on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Course_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
